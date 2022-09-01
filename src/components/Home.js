6b2ce8094bbd82df2fea8716536ebcd65ebbc453
lowlyphe <jones.kyle2893@gmail.com>
2022-09-01 11:41:07 -0400

    added dummy components for homes

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,19 +5,26 @@ import HomePreview from './HomePreview';
 export default function Home() {
 
   const [currentHomes, setCurrentHomes] = useState([]);
+  const [randomPicture, setRandomPicture] = useState()
 
     useEffect(() => {
         axios.get('http://localhost:3004/homes').then(res => {
             for (let i = 0; i < res.data.length; i++) {
-              setCurrentHomes(prevCurrentHomes => [...prevCurrentHomes, res.data])
+              setCurrentHomes(prevCurrentHomes => [...prevCurrentHomes, res.data[i]])
             }
         })
     }, [])
 
+    useEffect(() => {
+      axios.get('<img src="https://random.imagecdn.app/500/150">').then(res => {
+        console.log(res)
+        setRandomPicture(res.data)
+      })
+    }, [])
+
   return (
-    <div>
-      <p>Here</p>
-      {currentHomes.map((currentHomes) => <HomePreview city={currentHomes.city} state={currentHomes.state} />)}
+    <div className='flex flex-wrap justify-evenly items-center'>
+      {currentHomes.map((currentHomes) => <HomePreview city={currentHomes.city} state={currentHomes.state}  picture={randomPicture}/>)}
     </div>
   )
 }
