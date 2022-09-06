3dd05a371fe71de09ab51a2a14355f2149fe615e
Reaveskev <Reaveskev@gmail.com>
2022-09-06 15:14:47 -0700

    nicer

--- src/components/navbar/SearchBar.js
+++ src/components/navbar/SearchBar.js
@@ -91,101 +91,109 @@ export default function SearchBar() {
         {/************************************************************** */}
 
         <div className="flex-initial">
+          {showDropDown ? (
+            <div
+              style={{
+                backgroundColor: "red",
+                height: "auto",
+                width: 260,
+                position: "absolute",
+                display: "flex",
+                position: "absolute",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                top: 80,
+                backgroundColor: "white",
+                borderRadius: "5px",
+                boxShadow: " rgb(0 0 0 / 12%) 0px 6px 16px",
+              }}
+            >
+              <span
+                style={{
+                  padding: 10,
+                }}
+              >
+                Messages
+              </span>
+              <span
+                style={{
+                  padding: 10,
+                }}
+              >
+                Trips
+              </span>
+              <span
+                style={{
+                  padding: 10,
+                  cursor: "pointer",
+                }}
+                onClick={() => {
+                  navigate(`/wishlists`);
+                }}
+              >
+                Wishlists
+              </span>
+              <div
+                style={{
+                  borderBottom: "0.5px solid rgb(205, 202, 202)",
+                  borderTop: "0.5px solid rgb(205, 202, 202)",
+                  display: "flex",
+                  width: "auto",
+                  width: 260,
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                }}
+              >
+                <span
+                  style={{
+                    padding: 10,
+                  }}
+                >
+                  Host your Home
+                </span>
+                <span
+                  style={{
+                    padding: 10,
+                  }}
+                >
+                  Host an experience
+                </span>
+                <span
+                  style={{
+                    padding: 10,
+                  }}
+                >
+                  Refer a Host
+                </span>
+                <span
+                  style={{
+                    padding: 10,
+                  }}
+                >
+                  Account
+                </span>
+              </div>
+              <span
+                style={{
+                  padding: 10,
+                }}
+              >
+                Help
+              </span>
+              <span
+                style={{
+                  padding: 10,
+                }}
+              >
+                Logout
+              </span>
+            </div>
+          ) : null}
           <div className="flex justify-end items-center relative">
             <div className="flex mr-4 items-center">
               <a className="inline-block py-2 px-3 rounded-full" href="/">
                 <div className="flex items-center relative cursor-pointer whitespace-nowrap">
                   Become a host
-                  {showDropDown ? (
-                    <div
-                      style={{
-                        backgroundColor: "red",
-                        height: "auto",
-                        width: 260,
-                        position: "absolute",
-                        display: "flex",
-                        position: "absolute",
-                        flexDirection: "column",
-                        alignItems: "flex-start",
-                        top: 60,
-                        backgroundColor: "white",
-                        borderRadius: "5px",
-                        boxShadow: " rgb(0 0 0 / 12%) 0px 6px 16px",
-                      }}
-                      onClick={() => {
-                        setShowDropDown((showDropDown) => !showDropDown);
-                      }}
-                    >
-                      <span
-                        style={{
-                          padding: 10,
-                        }}
-                      >
-                        Messages
-                      </span>
-                      <span
-                        style={{
-                          padding: 10,
-                        }}
-                      >
-                        Trips
-                      </span>
-                      <span
-                        style={{
-                          padding: 10,
-                        }}
-                      >
-                        Wishlists
-                      </span>
-                      <div
-                        style={{
-                          borderBottom: "0.5px solid rgb(205, 202, 202)",
-                          borderTop: "0.5px solid rgb(205, 202, 202)",
-                          display: "flex",
-                          width: "auto",
-                          width: 260,
-                          flexDirection: "column",
-                          alignItems: "flex-start",
-                        }}
-                      >
-                        <span
-                          style={{
-                            padding: 10,
-                          }}
-                        >
-                          Host your Home
-                        </span>
-                        <span
-                          style={{
-                            padding: 10,
-                          }}
-                        >
-                          Refer a Host
-                        </span>
-                        <span
-                          style={{
-                            padding: 10,
-                          }}
-                        >
-                          Account
-                        </span>
-                      </div>
-                      <span
-                        style={{
-                          padding: 10,
-                        }}
-                      >
-                        Help
-                      </span>
-                      <span
-                        style={{
-                          padding: 10,
-                        }}
-                      >
-                        Logout
-                      </span>
-                    </div>
-                  ) : null}
                 </div>
               </a>
               <div className="block relative">
